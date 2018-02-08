a45c8e42df2044ad03f622ce66dc611d416b7352
Jonathan Herring <jonathanherring@Jonathans-MacBook-Air.local>
2018-02-08 00:54:47 -0700

    charts

--- skycast/src/components/history.js
+++ skycast/src/components/history.js
@@ -7,38 +7,72 @@ import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'r
     if (!history.hourly ) {
         return <section />
       } 
-    const data = []
-    function organize() {
+    const tempData = []
+    const windData = []
+
+    function organizeTemp() {
       history.hourly.data.map(hour => {
-        data.push({
-          name: Moment.unix(hour.time).format('h, a'),
+        tempData.push({
+          name: Moment.unix(hour.time).format('h a'),
           temperature: Math.round(hour.temperature)
         })
       })
-      return data
+      return tempData
     }
-    organize()
+    function organizeWind() {
+      history.hourly.data.map(hour => {
+        windData.push({
+          name: Moment.unix(hour.time).format('h a'),
+          windSpeed: hour.windSpeed
+        })
+      })
+      console.log('wind speeds hourly', windData)
+      return windData
+    }
+
+    organizeTemp()
+    organizeWind()
 
     return (
       <div>
         <div>
           <div>
             <LineChart
-              width={700}
+              width={800}
               height={200}
-              data={data}
+              data={tempData}
               margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
             >
               <XAxis dataKey="name" />
               <YAxis />
-              <CartesianGrid strokeDasharray="3 3" />
+              <CartesianGrid strokeDasharray="3 3 3" />
               <Tooltip />
               <Legend />
               <Line type="monotone" dataKey="temperature" stroke="#d82e22" />
             </LineChart>
           </div>
         </div>
+
+        <div>
+          <div>
+            <LineChart
+              width={800}
+              height={200}
+              data={windData}
+              margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+            >
+              <XAxis dataKey="name" />
+              <YAxis />
+              <CartesianGrid strokeDasharray="3" />
+              <Tooltip />
+              <Legend />
+              <Line type="monotone" dataKey="windSpeed" stroke="#d82e22" />
+            </LineChart>
+          </div>
+        </div>
       </div>
+
+      
     )
   }
 
